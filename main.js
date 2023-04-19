19cdc2a602817863c671705dbeaaa7098106912f
c.williams <cal.a.williams@hotmail.com>
2023-04-19 12:54:27 +0100

    Simplified text crawl

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-class EverythingMe {
-
-    _setTextNum = 0;
-
-    set textNum(value) {
-        if (this._setTextNum === value) this._setTextNum = 0;
-        this._setTextNum = value;
-    }
-
-    get textNum() {
-        return this._setTextNum;
-    }
-}
-
-const everythingMe = new EverythingMe();
 const sdText = {
     visible: document.getElementById('sd-text-visible'),
     hidden: document.getElementById('sd-text-hidden')
 }
+
+const sdString = [
+    'softwaredeveloper',
+    'SOFTWAREDEVELOPER',
+    'softwareDeveloper',
+    'SoftwareDeveloper',
+    'software_developer',
+    'SOFTWARE_DEVELOPER',
+    'software_Developer',
+    'software-developer',
+    'SOFTWARE-DEVELOPER',
+    'Software-Developer',
+    'Software Developer'
+];
 
 document.body.onload = () => init();
 
@@ -28,22 +27,21 @@ function textRotation() {
 
     setInterval( () => {
 
-        everythingMe.textNum = Math.floor(Math.random() * 11);
+        const rotNum = Math.floor(Math.random() * 11)
 
-        setCase(sdText.hidden, everythingMe.textNum);
+        sdText.hidden.textContent = sdString[rotNum];
         moveSDText();
         setTimeout(() => {
-            
-            setCase(sdText.visible, everythingMe.textNum);
-            sdText.visible.classList = '';
+
+            sdText.visible.textContent = sdString[rotNum];
+            sdText.visible.classList.remove('sd-text-moving');
             sdText.visible.style.transform = 'translateX(0vw)';
-            sdText.hidden.classList = '';
+            sdText.hidden.classList.remove('sd-text-moving');
             sdText.hidden.style.transform = 'translateX(0vw)';
 
-        }, 1100)
-        sdText.visible.classList = 'sd-text-moving';
-        sdText.hidden.classList = 'sd-text-moving';
-
+            }, 1100)
+        sdText.visible.classList.add('sd-text-moving');
+        sdText.hidden.classList.add('sd-text-moving');
 
     }, 5000)
 
@@ -53,61 +51,5 @@ function moveSDText() {
 
     sdText.hidden.style.transform = 'translateX(100vw)';
     sdText.visible.style.transform = 'translateX(100vw)';
-    // sdText.visible.style.opacity = 0;
-
-}
-
-function setCase(textToChange, caseNum) {
-
-    switch(caseNum) {
-
-        case 1:
-            textToChange.textContent = 'softwaredeveloper'
-            break;
-
-        case 2:
-            textToChange.textContent = 'SOFTWAREDEVELOPER'
-            break;
-
-        case 3:
-            textToChange.textContent = 'softwareDeveloper'
-            break;
-
-        case 4:
-            textToChange.textContent = 'SoftwareDeveloper'
-            break;
-
-        case 5:
-            textToChange.textContent = 'software_developer'
-            break;
-
-        case 6:
-            textToChange.textContent = 'SOFTWARE_DEVELOPER'
-            break;
-
-        case 7:
-            textToChange.textContent = 'software_Developer'
-            break;
-
-        case 8:
-            textToChange.textContent = 'Software_Developer'
-            break;
-
-        case 9:
-            textToChange.textContent = 'software-developer'
-            break;
-
-        case 10:
-            textToChange.textContent = 'SOFTWARE-DEVELOPER'
-            break;
-
-        case 11:
-            textToChange.textContent = 'Software-Developer'
-            break;
-    
-        case 0:
-            textToChange.textContent = 'Software Developer'
-            break;
-    }
 
 }
